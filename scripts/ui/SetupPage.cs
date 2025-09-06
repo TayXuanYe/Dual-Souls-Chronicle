caac4bc7cf4e23c7ef3cf16e575fcbced6af382f
TayXuanYe <pikaqiu04082004@gmail.com>
2025-09-06 14:56:36 +0800

    temp commit

--- scripts/ui/SetupPage.cs
+++ scripts/ui/SetupPage.cs
@@ -4,19 +4,27 @@ using System;
 public partial class SetupPage : Control
 {
 	private int _id;
+	private string _videoId;
 	[Export] private TextEdit _videoIdInput;
 	[Export] private Label _urlInputErrorLabel;
 	[Export] private Button _submitButton;
 
-	private string _videoId;
 	public void Init(int id)
 	{
 		_id = id;
 		_submitButton.Pressed += OnSubmitButtonPressed;
 	}
-	
+
 	private void OnSubmitButtonPressed()
 	{
-		
+		if (string.IsNullOrWhiteSpace(_videoIdInput.Text))
+		{
+			_urlInputErrorLabel.Text = "Video Id cannot be null or empty.";
+			return;
+		}
+		else
+		{
+			_urlInputErrorLabel.Text = "";
+		}
 	}
 }
