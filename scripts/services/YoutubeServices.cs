74cd272c99f996d07cc3995294e39ca75ff87c0b
TayXuanYe <pikaqiu04082004@gmail.com>
2025-09-06 15:08:25 +0800

    temp commit

---
+++ scripts/services/YoutubeServices.cs
@@ -0,0 +1,7 @@
+public class YoutubeServices
+{
+    public static string YoutubeApiKey { get; private set; }
+    public string LiveId { get; set; }
+    public bool IsGetData { get; set; }
+    public int Delay { get; set; }
+}
