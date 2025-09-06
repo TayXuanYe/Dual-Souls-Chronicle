2c7a70b4eecc8ee078c29c34be5608829bd985f5
TayXuanYe <pikaqiu04082004@gmail.com>
2025-09-06 20:49:15 +0800

    Add count down display

--- scenes/ui/GameScene.cs
+++ scenes/ui/GameScene.cs
@@ -3,19 +3,6 @@ using System;
 
 public partial class GameScene : Control
 {
-	[Export] private Label _voitingTimeLabel;
-	[Export] private Label _voteCountLabel1;
-	[Export] private Label _voteCountLabel2;
-	[Export] private Label _voteCountLabel3;
-	[Export] private Panel _voteBarPanel1;	
-	[Export] private Panel _voteBarPanel2;	
-	[Export] private Panel _voteBarPanel3;	
-	
-	private int _totalVoteCount;
-	private int _vote1Count;
-	private int _vote2Count;
-	private int _vote3Count;
-	private double _currentTime;
 	public override void _Ready()
 	{
 		
---
+++ scenes/ui/SelectScenes.cs
@@ -0,0 +1,65 @@
+using Godot;
+using System;
+
+public partial class SelectScenes : VBoxContainer
+{
+	[Export] private Label _votingTimeLabel;
+	[Export] private Label _voteCountLabel1;
+	[Export] private Label _voteCountLabel2;
+	[Export] private Label _voteCountLabel3;
+	[Export] private Panel _voteBarPanel1;
+	[Export] private Panel _voteBarPanel2;
+	[Export] private Panel _voteBarPanel3;
+
+	private int _totalVoteCount;
+	private int _vote1Count;
+	private int _vote2Count;
+	private int _vote3Count;
+	private double _voteTimeCountdown;
+	private double _updateTime;
+	bool isInit = false;
+	public override void _Ready()
+	{
+		this.Visible = false;
+	}
+
+	public override void _Process(double delta)
+	{
+		if (!isInit) { return; }
+		DisplayVotingTime(delta);
+
+	}
+
+	public void Init(double voteTime)
+	{
+		this.Visible = true;
+		_voteTimeCountdown = voteTime;
+
+		isInit = true;
+	}
+
+	public void UpdateVoteCount(int vote1Count, int vote2Count, int vote3Count, double updateTime)
+	{
+
+	}
+
+	private void DisplayVotingTime(double delta)
+	{
+		if (_voteTimeCountdown < 0)
+		{
+			_votingTimeLabel.Text = $"Voting Time: 0s";
+			return;
+		}
+		_votingTimeLabel.Text = $"Voting Time: {_voteTimeCountdown}s";
+
+		if (_voteTimeCountdown <= 10f)
+		{
+			if (Math.Floor(_voteTimeCountdown) % 2 == 0)
+				_votingTimeLabel.Modulate = new Color("#FF0000");
+			else
+				_votingTimeLabel.Modulate = new Color("#FFFFFF");
+		}
+
+		_voteTimeCountdown -= delta;
+	}
+}
