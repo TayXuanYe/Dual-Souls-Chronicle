664facfe1f838677945f3f87fbeafc6e84308c54
TayXuanYe <pikaqiu04082004@gmail.com>
2025-09-06 13:30:26 +0800

    fix get config code

--- scripts/Main.cs
+++ scripts/Main.cs
@@ -1,5 +1,6 @@
 using Godot;
-using System;
+using System.IO;
+using System.Text.Json;
 
 public partial class Main : Control
 {
@@ -11,34 +12,35 @@ public partial class Main : Control
 	{
 		// get api key
 		string configPath = "res://config.json";
+		Config config = null;
 		 try
-        {
-            string jsonString = File.ReadAllText(ProjectSettings.GlobalizePath(configPath));
-            config = JsonSerializer.Deserialize<Config>(jsonString, new JsonSerializerOptions
-            {
-                PropertyNameCaseInsensitive = true
-            });
-        }
-        catch (FileNotFoundException)
-        {
-            GD.PrintErr($"Error: Config not found: {configPath}");
-            GetTree().Quit();
-            return;
-        }
-        catch (JsonException e)
-        {
-            GD.PrintErr($"Error: Failed to parse the configuration file. Please check if the format of {configPath} is correct. Details: {e.Message}");
-            GetTree().Quit();
-            return;
-        }
+		{
+			string jsonString = File.ReadAllText(ProjectSettings.GlobalizePath(configPath));
+			config = JsonSerializer.Deserialize<Config>(jsonString, new JsonSerializerOptions
+			{
+				PropertyNameCaseInsensitive = true
+			});
+		}
+		catch (FileNotFoundException)
+		{
+			GD.PrintErr($"Error: Config not found: {configPath}");
+			GetTree().Quit();
+			return;
+		}
+		catch (JsonException e)
+		{
+			GD.PrintErr($"Error: Failed to parse the configuration file. Please check if the format of {configPath} is correct. Details: {e.Message}");
+			GetTree().Quit();
+			return;
+		}
 
-        if (config == null || string.IsNullOrWhiteSpace(config.YoutubeApiKey))
-        {
-            GD.PrintErr("Error: The configuration file content is invalid, API Key is missing.");
-            GetTree().Quit();
-            return;
-        }
-        GD.Print("Configuration read successfully, API Key loaded.");
-        youtube_api_key = config.YoutubeApiKey;
+		if (config == null || string.IsNullOrWhiteSpace(config.YoutubeApiKey))
+		{
+			GD.PrintErr("Error: The configuration file content is invalid, API Key is missing.");
+			GetTree().Quit();
+			return;
+		}
+		GD.Print("Configuration read successfully, API Key loaded.");
+		youtube_api_key = config.YoutubeApiKey;
 	}
 }
