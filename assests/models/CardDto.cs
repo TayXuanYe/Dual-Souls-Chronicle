4f59bc687ed99fc605d56428c1f8421a690f89fc
TayXuanYe <pikaqiu04082004@gmail.com>
2025-09-07 14:06:09 +0800

    Add card manager for manage and persistence card data

--- assests/models/CardDto.cs
+++ assests/models/CardDto.cs
@@ -4,6 +4,13 @@ using System;
 public class CardDto
 {
     public string Name { get; set; }
-    public TextureRect ImageTexture { get; set; }
+    public Texture2D ImageTexture { get; set; }
     public string Describe;
+
+    public CardDto(string name, Texture2D imageTexture, string describe)
+    {
+        Name = name;
+        ImageTexture = imageTexture;
+        Describe = describe;
+    }
 }
