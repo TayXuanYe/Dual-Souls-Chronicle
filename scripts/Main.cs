1dfdacc9b19a1f518aa77425fed1b0d823a92440
TayXuanYe <pikaqiu04082004@gmail.com>
2025-09-07 18:06:46 +0800

    Un comment some things temp comment to avoid some function

--- scripts/Main.cs
+++ scripts/Main.cs
@@ -16,48 +16,48 @@ public partial class Main : Control
 	public override void _Ready()
 	{
 		// get api key
-		// string configPath = "res://config.json";
-		// Config config = null;
-		// try
-		// {
-		// 	string jsonString = File.ReadAllText(ProjectSettings.GlobalizePath(configPath));
-		// 	config = JsonSerializer.Deserialize<Config>(jsonString, new JsonSerializerOptions
-		// 	{
-		// 		PropertyNameCaseInsensitive = true
-		// 	});
-		// }
-		// catch (FileNotFoundException)
-		// {
-		// 	GD.PrintErr($"Error: Config not found: {configPath}");
-		// 	GetTree().Quit();
-		// 	return;
-		// }
-		// catch (JsonException e)
-		// {
-		// 	GD.PrintErr($"Error: Failed to parse the configuration file. Please check if the format of {configPath} is correct. Details: {e.Message}");
-		// 	GetTree().Quit();
-		// 	return;
-		// }
+		string configPath = "res://config.json";
+		Config config = null;
+		try
+		{
+			string jsonString = File.ReadAllText(ProjectSettings.GlobalizePath(configPath));
+			config = JsonSerializer.Deserialize<Config>(jsonString, new JsonSerializerOptions
+			{
+				PropertyNameCaseInsensitive = true
+			});
+		}
+		catch (FileNotFoundException)
+		{
+			GD.PrintErr($"Error: Config not found: {configPath}");
+			GetTree().Quit();
+			return;
+		}
+		catch (JsonException e)
+		{
+			GD.PrintErr($"Error: Failed to parse the configuration file. Please check if the format of {configPath} is correct. Details: {e.Message}");
+			GetTree().Quit();
+			return;
+		}
 
-		// if (config == null || string.IsNullOrWhiteSpace(config.YoutubeApiKey))
-		// {
-		// 	GD.PrintErr("Error: The configuration file content is invalid, API Key is missing.");
-		// 	GetTree().Quit();
-		// 	return;
-		// }
-		// GD.Print("Configuration read successfully, API Key loaded.");
-		// YoutubeApiKey = config.YoutubeApiKey;
+		if (config == null || string.IsNullOrWhiteSpace(config.YoutubeApiKey))
+		{
+			GD.PrintErr("Error: The configuration file content is invalid, API Key is missing.");
+			GetTree().Quit();
+			return;
+		}
+		GD.Print("Configuration read successfully, API Key loaded.");
+		YoutubeApiKey = config.YoutubeApiKey;
 
-		// // create setup page and add to sub viewpoint
-		// Node setupScene1 = _setupScene.Instantiate();
-		// _subViewport1.AddChild(setupScene1);
-		// if (setupScene1 is SetupPage setupPageScript1)
-		// 	setupPageScript1.Init(1);
+		 // create setup page and add to sub viewpoint
+		 Node setupScene1 = _setupScene.Instantiate();
+		 _subViewport1.AddChild(setupScene1);
+		 if (setupScene1 is SetupPage setupPageScript1)
+		 	setupPageScript1.Init(1);
 
 
-		// Node setupScene2 = _setupScene.Instantiate();
-		// _subViewport2.AddChild(setupScene2);
-		// if (setupScene2 is SetupPage setupPageScript2)
-		// 	setupPageScript2.Init(2);
+		 Node setupScene2 = _setupScene.Instantiate();
+		 _subViewport2.AddChild(setupScene2);
+		 if (setupScene2 is SetupPage setupPageScript2)
+		 	setupPageScript2.Init(2);
 	}
 }
