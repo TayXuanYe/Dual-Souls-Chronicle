3574636b9819d13d5d1776fa524363d6fbf3b170
TayXuanYe <pikaqiu04082004@gmail.com>
2025-09-11 10:25:19 +0800

    Add GamaProgressManager for game progress handling

---
+++ scripts/global/GamaProgressManager.cs
@@ -0,0 +1,104 @@
+using Godot;
+using System;
+using System.Collections.Generic;
+
+public partial class GamaProgressManager : Node
+{
+    public static GamaProgressManager Instance { get; set; }
+    private Dictionary<int, (string type, string resourcesPath)> progressList = new Dictionary<int, (string type, string resources)>();
+    public override void _Ready()
+    {
+        if (Instance != null)
+        {
+            QueueFree();
+            return;
+        }
+        Instance = this;
+        LoadGameProgress();
+    }
+
+    private void LoadGameProgress()
+    {
+        // select character
+        progressList.Add(0, ("SelectCharacter", "res://scenes/ui/select_scenes.tscn"));
+
+        // level 1
+
+        // select buff
+        progressList.Add(2, ("SelectBuff", "res://scenes/ui/select_scenes.tscn"));
+        // select rest
+
+        // level 2
+
+        // select buff
+        // select rest
+
+        // level 3 (boss)
+
+        // select boss buff
+        // select rest
+
+        // level 4 
+
+        // select buff
+        // select rest
+
+        // level 5
+        // select buff
+        // select rest
+
+        // level 6 (final boss)
+
+        // fail scene
+        // success scene
+    }
+
+    // this for select scene
+    public (bool IsValid, Node node) GetProgress(int index)
+    {
+        if (index >= progressList.Count)
+        {
+            return (false, null);
+        }
+
+        var data = progressList[index];
+        Node node = null;
+        switch (data.type)
+        {
+            case "SelectCharacter":
+                node = InitSelectCharacterScene(data.resourcesPath, index);
+                break;
+            case "SelectBuff":
+                node = InitSelectBuffScene(data.resourcesPath, index);
+                break;
+            // Add other cases as needed
+        }
+
+        bool isValid = node != null;
+        return (isValid, node);
+    }
+
+    private Node InitSelectCharacterScene(string resourcesPath, int seed)
+    {
+        Node node = GD.Load<PackedScene>(resourcesPath).Instantiate();
+        if (node is SelectScenes script)
+        {
+            string[] colors = ["#66CCFF", "#FF6666", "#66CC66"];
+            script.Init(60, 3, colors, "character", seed);
+        }
+
+        return node;
+    }
+
+    private Node InitSelectBuffScene(string resourcesPath, int seed)
+    {
+        Node node = GD.Load<PackedScene>(resourcesPath).Instantiate();
+        if (node is SelectScenes script)
+        {
+            string[] colors = ["#66CCFF", "#FF6666", "#66CC66"];
+            script.Init(20, 3, colors, "buff", seed);
+        }
+
+        return node;
+    }
+}
