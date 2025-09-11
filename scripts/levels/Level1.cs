0ae816857b57ec6edbc74bfd4061a7588f9b86f6
TayXuanYe <pikaqiu04082004@gmail.com>
2025-09-11 13:27:27 +0800

    level1 - empty

---
+++ scripts/levels/Level1.cs
@@ -0,0 +1,21 @@
+using Godot;
+using System;
+
+public partial class Level : Node
+{
+    
+
+    private void Process()
+    {
+        while (true)
+        {
+
+        }
+    }
+
+    public override void _Ready()
+    {
+        Process();
+    }
+
+}
